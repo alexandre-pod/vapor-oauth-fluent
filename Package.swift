ef21324134ba8e30e19a33fba96c87a61ad1eb31
alexandre-pod <24512899+alexandre-pod@users.noreply.github.com>
2023-05-15 23:03:41 +0200

    Update Package.swift and dependency for Vapor 4 and VaporOAuth 1.0

--- Package.swift
+++ Package.swift
@@ -1,9 +1,28 @@
+// swift-tools-version:5.6
+
 import PackageDescription
 
 let package = Package(
     name: "vapor-oauth-fluent",
+    platforms: [
+        .macOS(.v12)
+    ],
+    products: [
+        .library(name: "VaporOAuthFluent", targets: ["VaporOAuthFluent"]),
+    ],
     dependencies: [
-    	.Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
-    	.Package(url: "https://github.com/brokenhandsio/vapor-oauth.git", majorVersion: 0),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.76.0"),
+        .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.42.0"),
+        .package(url: "https://github.com/brokenhandsio/vapor-oauth", revision: "main"),
+    ],
+    targets: [
+        .target(
+            name: "VaporOAuthFluent",
+            dependencies: [
+                .product(name: "FluentKit", package: "fluent-kit"),
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "OAuth", package: "vapor-oauth"),
+            ]),
+        .testTarget(name: "VaporOAuthFluentTests", dependencies: ["VaporOAuthFluent"]),
     ]
 )
