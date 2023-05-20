3370e1699185f477e34b4aab989c08ed95baff14
alexandre-pod <24512899+alexandre-pod@users.noreply.github.com>
2023-05-21 01:45:43 +0200

    WIP Waiting on FluentResourceServerRetriever supporting async

--- Sources/VaporOAuthFluent/Managers/FluentResourceServerRetriever.swift
+++ Sources/VaporOAuthFluent/Managers/FluentResourceServerRetriever.swift
@@ -12,4 +12,9 @@ public struct FluentResourceServerRetriever: ResourceServerRetriever {
     public func getServer(_ username: String) -> OAuthResourceServer? {
         return try? FluentOAuthResourceServer.find(username, on: database).wait()?.oAuthResourceServer
     }
+
+    // TODO: Use this method once FluentResourceServerRetriever is migrated to use async
+//    public func getServer(_ username: String) async throws -> OAuthResourceServer? {
+//        return try await FluentOAuthResourceServer.find(username, on: database)?.oAuthResourceServer
+//    }
 }
